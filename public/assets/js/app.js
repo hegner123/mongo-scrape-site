83abe50b977921ab9b617e934f8ba62df966a113
Michael Hegner <hegner123@gmail.com>
2019-12-10 16:08:55 -0500

    made progress on homework

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -26,11 +26,11 @@ $(document).on("click", "p", function() {
       // The title of the article
       $("#notes").append("<h2>" + data.title + "</h2>");
       // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
+      $("#notes").append("<input id='titleinput' class='form-control mt-3' name='title' placeholder='title'>");
       // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      $("#notes").append("<textarea id='bodyinput' class='form-control mt-3' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote' class='btn btn-secondary mt-3'>Save Note</button>");
 
       // If there's a note in the article
       if (data.note) {
@@ -42,31 +42,57 @@ $(document).on("click", "p", function() {
     });
 });
 
-// When you click the savenote button
-$(document).on("click", "#savenote", function() {
-  // Grab the id associated with the article from the submit button
+
+$(document).on("click", "p", function() {
+  // Empty the notes from the note section
+  $("#notes").empty();
+  // Save the id from the p tag
   var thisId = $(this).attr("data-id");
 
-  // Run a POST request to change the note, using what's entered in the inputs
+  // Now make an ajax call for the Article
   $.ajax({
-    method: "POST",
-    url: "/articles/" + thisId,
-    data: {
-      // Value taken from title input
-      title: $("#titleinput").val(),
-      // Value taken from note textarea
-      body: $("#bodyinput").val()
-    }
+    method: "GET",
+    url: "/articles/" + thisId
   })
-    // With that done
+    // With that done, add the note information to the page
     .then(function(data) {
-      // Log the response
+      for (i=0;i<data.notes.length;i++){
+        $("#notes").append(`
+        <div class="notes-item mt-3">
+        <h2>`+data.notes[i].title+`</h2>
+        <p>`+data.notes[i].body+`</p>
+        </div>
+        `)
+      }
       console.log(data);
-      // Empty the notes section
-      $("#notes").empty();
+     
     });
-
-  // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
 });
+
+
+// When you click the savenote button
+
+
+
+$(document).on('click', '#savenote', function () {
+  var noteTitle = $("#titleinput");
+  var noteBody = $("#bodyinput");
+  var articleID = $("#savenote").attr("data-id");
+  console.log(articleID);
+  createNewNote(noteTitle.val().trim(), noteBody.val().trim(), articleID)
+  noteTitle.val('');
+  noteBody.val('');
+  
+})
+
+// Passes the task title, description, and associated article id to the post.
+function createNewNote(title, body, articleID) {
+  $.post("/articles/note", {
+    title: title,
+    body: body,
+    articleID: articleID
+  }).then(function (data) {
+    console.log(data);
+    window.location.href = data
+  })
+}
